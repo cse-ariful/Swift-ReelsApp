75af2f24b295cdc097033eb40cef4b1e7813903b
Ariful Jannat Arif <cse.ariful@gmail.com>
2023-01-05 21:09:43 +0600

    Initial Commit

---
+++ PexelsClient/PexelsClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsClientApp.swift
+//  PexelsClient
+//
+//  Created by Ariful Jannat Arif on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PexelsClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
